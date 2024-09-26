5acdd8218927cbe94b6974aa5ed26ea65303088f
Max Larsson <maxslarsson@gmail.com>
2024-09-26 00:52:45 -0400

    Add basic dashboard

---
+++ ground-server/src/components/dashboard/dashboard-widget.tsx
@@ -0,0 +1,31 @@
+import { forwardRef, type ReactNode, type MouseEvent, type TouchEvent, type CSSProperties } from 'react';
+
+import { cn } from '@/lib/utils';
+
+
+interface DashboardWidgetProps {
+  style?: CSSProperties
+  className?: string, 
+  onMouseDown?: (event: MouseEvent) => void, 
+  onMouseUp?: (event: MouseEvent) => void, 
+  onTouchEnd?: (event: TouchEvent) => void, 
+  children: ReactNode
+}
+
+
+export const DashboardWidget = forwardRef<HTMLDivElement, DashboardWidgetProps>(({
+  style,
+  className, 
+  onMouseDown, 
+  onMouseUp, 
+  onTouchEnd, 
+  children, 
+}, ref) => {
+  return (
+    <div style={style} className={cn("border border-gray-200 p-4", className)} ref={ref} onMouseDown={onMouseDown} onMouseUp={onMouseUp} onTouchEnd={onTouchEnd}>
+      {children} 
+    </div>
+  );
+})
+
+DashboardWidget.displayName = 'DashboardWidget'; 

0976b394621b2d5aaffb68031ae236ea5fe71c27
Zach Garcia <zachagarcia@gmail.com>
2024-10-14 13:41:19 -0400

    Make widgets more extensible

--- ground-server/src/lib/telemetry-channels.ts
+++ ground-server/src/lib/telemetry-channels.ts
@@ -1,12 +1,24 @@
 import TemperatureWidget from "@/components/widgets/temperature-widget";
+import GraphWidget from "@/components/widgets/graph-widget";
 
 import type { TelemetryChannel } from "@/lib/definitions";
 
 export const TELEMETRY_CHANNELS: TelemetryChannel[] = [
   {
     label: "Temperature",
+    unit: "°C",
     requiresAuth: false,
+    dbField: "temp",
     modes: ["Value", "Chart", "15m Chart", "60m Chart"],
+    data: [],
     component: TemperatureWidget,
+  },
+  {
+    label: "Solenoid Valve 1 Continuity",
+    requiresAuth: false,
+    dbField: "sv1_cont",
+    modes: ["Value", "Chart", "15m Chart", "60m Chart"],
+    data: [],
+    component: GraphWidget,
   },
 ];

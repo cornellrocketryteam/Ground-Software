49d801f2dd5f254a646578154c5f6dd12abea46b
Zach Garcia <zachagarcia@gmail.com>
2024-11-01 10:13:53 -0400

    Fix grpc

--- ground-server/src/app/grpc/commandClient.js
+++ ground-server/src/app/grpc/commandClient.js
@@ -19,11 +19,18 @@ var protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
 // The protoDescriptor object has the full package hierarchy
 var command = protoDescriptor.command;
 
-var client = new command.Commander('localhost:50051', grpc.credentials.createInsecure());
+var client = new command.Commander('fill-station:50051', grpc.credentials.createInsecure());
 
 export async function runSendCommand() {
   var command = {
-    temp: 'Goodbye'
+    sv1_open: false,
+    bv1_open: false,
+    bv1_off: false,
+    qd_retract: false,
+    ignite: false,
+    sv2_close: false,
+    mav_open: false,
+    fire: false
   };
   console.log(command);
 

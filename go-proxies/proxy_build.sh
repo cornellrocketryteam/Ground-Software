276c6505f399dee96871e3d351086ef9be9daf17
bwzsilicon <bwz5@cornell.edu>
2024-11-14 16:56:42 -0500

    Creating Rocket Telemetry Go Proxy

--- go-proxies/proxy_build.sh
+++ go-proxies/proxy_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Run from telemetry-proxy directory
+# Run from go-proxy directory
 
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
@@ -8,10 +8,14 @@ export PATH="$PATH:$(go env GOPATH)/bin"
 protoc --go_out=./proto-out --go_opt=paths=source_relative --go-grpc_out=./proto-out --go-grpc_opt=paths=source_relative -I../protos ../protos/command.proto --experimental_allow_proto3_optional
 pushd fill-telem-proxy
 go build main.go
+popd 
+pushd rocket-telem-proxy
+go build main.go
 popd && pushd websocket-proxy
 go build main.go
 popd
 pushd ..
 docker build -t ghcr.io/cornellrocketryteam/fill-telem-proxy:latest -f go-proxies/fill-telem-proxy/Dockerfile . &
+docker build -t ghcr.io/cornellrocketryteam/rocket-telem-proxy:latest -f go-proxies/rocket-telem-proxy/Dockerfile . &
 docker build -t ghcr.io/cornellrocketryteam/websocket-proxy:latest -f go-proxies/websocket-proxy/Dockerfile .
 popd

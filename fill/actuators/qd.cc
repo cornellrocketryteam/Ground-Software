f096cffb1b52f06d00856cefbf78a764d303cedb
Benjamin Zou <benjamin@siliconassurance.com>
2025-03-18 18:32:19 -0400

    Using the pull down resistor on PUL_PIN

--- fill/actuators/qd.cc
+++ fill/actuators/qd.cc
@@ -9,6 +9,10 @@ QD::QD(){
     pinMode(DIR_PIN, OUTPUT);
     pinMode(ENA_PIN, OUTPUT);
 
+    pullUpDnControl(PUL_PIN, PUD_DOWN);
+    pullUpDnControl(DIR_PIN, PUD_DOWN);
+    pullUpDnControl(ENA_PIN, PUD_DOWN);
+
     // Disable the driver by setting the enable pin low
     digitalWrite(ENA_PIN, LOW);
     // pulse should be default high
@@ -38,12 +42,12 @@ void QD::turnMotor(){
     digitalWrite(ENA_PIN, HIGH);
     delay(201); // necessary to set enable correctly 
     
-    for (int step = 0; step < 1200; step++) {
+    for (int step = 0; step < 7000; step++) {
         // Generate one pulse with 50% duty cycle
         digitalWrite(PUL_PIN, LOW);  
-        delay(10);             
+        delay(2);             
         digitalWrite(PUL_PIN, HIGH);  
-        delay(10);                
+        delay(2);                
     }
 
     digitalWrite(ENA_PIN, LOW);
